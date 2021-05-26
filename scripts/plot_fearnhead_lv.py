2b69f0e542579ce0291a526e8c053641c82e7026
yannikschaelte <yannik.schaelte@gmail.com>
2021-05-26 12:18:00 +0200

    add plotting scripts

---
+++ scripts/plot_fearnhead_lv.py
@@ -0,0 +1,33 @@
+import os
+import matplotlib.pyplot as plt
+
+import slad
+import pyabc
+
+pyabc.settings.set_figure_params("pyabc")
+
+labels = [
+    "Euclidean",
+    "Calibrated",
+    # for scale function comparison
+    "Adaptive_std",
+    "Adaptive",
+    "Euclidean_Linear",
+    "Linear_initial",
+    "Linear",
+    "Linear_Subset",
+    "MS",
+    "MS_Subset",
+    # info distances
+    "Info",
+    "Info_Subset",
+]
+
+iters = list(range(3))
+problem = slad.FearnheadLVProblem()
+
+slad.plot_cis(problem, labels, iters)
+plt.savefig(f"plot/{problem.get_id()}_cis.png")
+
+slad.plot_1d_kdes(problem, labels, iters)
+plt.savefig(f"plot/{problem.get_id()}_1d_kdes.png")
